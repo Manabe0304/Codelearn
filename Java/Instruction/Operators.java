110db3c35a17f743566f6bbb2633b1a6314f6794
Manabe0304 <hainamn833@gmail.com>
2024-08-09 09:03:27 +0700

    Update Operators.java

--- Java/Instruction/Operators.java
+++ Java/Instruction/Operators.java
@@ -3,8 +3,12 @@ public class HelloWorld
    public static void main(String[] args) 
    {
        System.out.println(313 + 122);
+       System.out.println("313 + 122 = " + (313 + 122));
        System.out.println(531 - 327);
+       System.out.println("531 - 327 = " + (531 - 327));
        System.out.println(37 * 56);
+       System.out.println("37 * 56 = " + (37 * 56));
        System.out.println(32 / 4);
+       System.out.println("32 / 4 = " + (32 / 4));
    }
 }
