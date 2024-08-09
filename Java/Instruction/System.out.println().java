c653b06938a3a2d3ae581ba306e8992afbfe9697
Manabe0304 <hainamn833@gmail.com>
2024-08-09 08:52:47 +0700

    Update System.out.println().java

--- Java/Instruction/System.out.println().java
+++ Java/Instruction/System.out.println().java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main(String[]args) {
 		System.out.println("Welcome to Codelearn.io!");
+		System.out.println("Name: Codelearn\nDate of birth: 2019");
 	}
 }
