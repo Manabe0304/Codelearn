3fc9ab4303c630bdc3162363aa5183b3e002fd7b
Manabe0304 <hainamn833@gmail.com>
2024-08-09 08:52:15 +0700

    Update Operators.java

--- Java/Instruction/Operators.java
+++ Java/Instruction/Operators.java
@@ -3,5 +3,8 @@ public class HelloWorld
    public static void main(String[] args) 
    {
        System.out.println(313 + 122);
+       System.out.println(531 - 327);
+       System.out.println(37 * 56);
+       System.out.println(32 / 4);
    }
 }
