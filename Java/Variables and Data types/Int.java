e3a5c0f4c9120c34e623a0106730797e2527ee26
Manabe0304 <hainamn833@gmail.com>
2024-08-19 21:49:29 +0700

    Update Int.java

--- Java/Variables and Data types/Int.java
+++ Java/Variables and Data types/Int.java
@@ -6,13 +6,5 @@ public class Variable {
         System.out.println("a - b = " + (a - b));
         System.out.println("a * b = " + (a * b));
         System.out.println("a / b = " + (a / b));
-
-	double c = 10.5;
-        double d = 7;
-        System.out.println("c / d = " + c / d);     
-
-	double length = 7.5;
-	double width = 3.8;
-	System.out.println("Area = " + length * width);     
      }
 }
