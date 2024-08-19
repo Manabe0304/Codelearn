c24a1b3e2a6be3af405a6da779393dddafa5a4cf
Manabe0304 <hainamn833@gmail.com>
2024-08-19 21:50:47 +0700

    Create Float & Double.java

---
+++ Java/Variables and Data types/Float & Double.java
@@ -0,0 +1,11 @@
+public class Variable {
+     public static void main(String[] args) {
+	      double c = 10.5;
+        double d = 7;
+        System.out.println("c / d = " + c / d);     
+
+      	double length = 7.5;
+	      double width = 3.8;
+	      System.out.println("Area = " + length * width);     
+     }
+}
