a17c232120c8bca6c06bfa1ab020b3fd24f15282
Manabe0304 <hainamn833@gmail.com>
2024-08-19 21:51:16 +0700

    Update Float & Double.java

--- Java/Variables and Data types/Float & Double.java
+++ Java/Variables and Data types/Float & Double.java
@@ -1,10 +1,10 @@
 public class Variable {
      public static void main(String[] args) {
 	      double c = 10.5;
-        double d = 7;
-        System.out.println("c / d = " + c / d);     
+              double d = 7;
+              System.out.println("c / d = " + c / d);     
 
-      	double length = 7.5;
+      	      double length = 7.5;
 	      double width = 3.8;
 	      System.out.println("Area = " + length * width);     
      }
