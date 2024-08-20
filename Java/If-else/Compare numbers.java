3feeb707c460ca84fd571ee672507da5278c1967
Manabe0304 <hainamn833@gmail.com>
2024-08-20 23:01:44 +0700

    Update Compare numbers.java

--- Java/If-else/Compare numbers.java
+++ Java/If-else/Compare numbers.java
@@ -12,9 +12,9 @@ public class Statement {
 		}
 
 		if (a != 0 && b != 0) {
-	                System.out.println("a is not equal to 0 and b is not equal to 0");
+	        System.out.println("a is not equal to 0 and b is not equal to 0");
 		} else {
-	    	        System.out.println("a is equal to 0 or b is equal to 0");
-	        }
+	    	System.out.println("a is equal to 0 or b is equal to 0");
+	    }
     }
 }
