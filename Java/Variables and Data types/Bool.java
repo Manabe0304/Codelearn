6b41d47f0faca9ea8a62e728588e5a9b49d9f999
Manabe0304 <hainamn833@gmail.com>
2024-08-20 09:36:48 +0700

    Create Bool.java

---
+++ Java/Variables and Data types/Bool.java
@@ -0,0 +1,16 @@
+public class Variable {
+	public static void main(String[] args) {
+		// Khai bao bien a kieu boolean va gan gia tri cho a = true
+		boolean a = true;
+		// Khai bao bien a kieu boolean va gan gia tri cho b = false
+		boolean b = false;
+		System.out.println(a);
+		System.out.println(b);
+
+		int x = 5;
+		int y = 4;
+
+		boolean z = a > b;
+		System.out.println(z);
+	}
+}
