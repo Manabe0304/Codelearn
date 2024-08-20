25cff20bad733e4fd4745e0555abdb03d001d6af
Manabe0304 <hainamn833@gmail.com>
2024-08-20 09:09:23 +0700

    Create Long.java

---
+++ Java/Variables and Data types/Long.java
@@ -0,0 +1,11 @@
+public class Variable {
+	public static void main(String[] args) {
+		long a = 384847522;
+		long b = 988347273;
+		System.out.println(a * b);
+
+		int c = 384847522;
+		int d = 988347273;
+		System.out.println((long) c * d);
+	}
+}
