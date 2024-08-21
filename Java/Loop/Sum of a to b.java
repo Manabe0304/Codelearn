281df3727ef1a8346a65790e6a19fd6dcc85126d
Manabe0304 <hainamn833@gmail.com>
2024-08-21 11:43:50 +0700

    Update Sum of a to b.java

--- Java/Loop/Sum of a to b.java
+++ Java/Loop/Sum of a to b.java
@@ -5,6 +5,11 @@ public class Loop {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
+		for (int i = a; i <= b; i++) {
+			System.out.print(i + " ");
+		}
+		
+		System.out.println();
 		int answer = 0;
 		for (int i = a; i <= b; i++) {
 			// answer += i means answer = answer + i
