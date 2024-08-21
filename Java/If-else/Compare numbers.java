b4417987bd6b27a4266c7df1939cce638e1ee4df
Manabe0304 <hainamn833@gmail.com>
2024-08-21 10:14:22 +0700

    Update Compare numbers.java

--- Java/If-else/Compare numbers.java
+++ Java/If-else/Compare numbers.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Statement {
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		if (a >= b) {
