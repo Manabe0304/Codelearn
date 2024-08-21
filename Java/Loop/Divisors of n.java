31cf8c5dc741c6c70f05e241126effcfc9d25671
Manabe0304 <hainamn833@gmail.com>
2024-08-21 18:22:23 +0700

    Update Divisors of n.java

--- Java/Loop/Divisors of n.java
+++ Java/Loop/Divisors of n.java
@@ -4,10 +4,18 @@ public class Loop {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int answer = 0;
 		for (int i = 1; i <= n; i++) {
 			if (n % i == 0) {
 				System.out.print(i + " ");
 			}
 		}
+		System.out.println();
+		for (int i = 1; i <= n; i++) {
+			if(n % i == 0) {
+				answer++;
+			}
+		}
+		System.out.println(answer);
 	}
 }
