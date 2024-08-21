fbbc32edb4276b1cde8009a69ad5fff94bdb04b2
Manabe0304 <hainamn833@gmail.com>
2024-08-21 19:03:48 +0700

    Create Break statement.java

---
+++ Java/Do & Do-While/Break statement.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int i = 1;
+        
+        while (i <= n) {
+            if (i == k) {
+                break;
+            }
+            System.out.print(i + " ");
+            i++; // Increment i manually
+        }
+    }
+}
