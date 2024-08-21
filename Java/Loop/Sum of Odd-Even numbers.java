636131dc19bcd7671a29c65a5666b11ac971ad1b
Manabe0304 <hainamn833@gmail.com>
2024-08-21 11:35:59 +0700

    Update and rename Sum of Odd numbers.java to Sum of Odd-Even numbers.java

--- Java/Loop/Sum of Odd-Even numbers.java
+++ Java/Loop/Sum of Odd-Even numbers.java
@@ -11,5 +11,15 @@ public class Loop {
 			}
 		}
 		System.out.println(answer);
+		
+		System.out.println();
+		for (int i = 0; i <= n; i++) {
+			if (i % 2 == 0) {
+				answer += i;
+			}
+		}
+		System.out.println(answer);
+		
+		
 	}
 }
