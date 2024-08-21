e3bc53dfa9cdb41c2228233519fceb70d1ac3467
Manabe0304 <hainamn833@gmail.com>
2024-08-21 22:04:21 +0700

    Create Continue.java

---
+++ Java/Do & Do-While/Continue.java
@@ -0,0 +1,36 @@
+//Cach 1
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+			        if (i % k == 0) {
+				          continue;
+			        }
+			  System.out.print(i + " ");
+		    }
+	}
+}
+
+//Cach 2
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+		    int i = 1;
+		    while (i <= n) {
+		         if (i % k == 0) {
+		             i++;
+		             continue;
+		         }
+		    System.out.print(i + " ");
+		    i++;
+		}
+	}
+}
