1b36f734c98ccc83690708f00d64167a8210fe98
Manabe0304 <hainamn833@gmail.com>
2024-08-21 19:06:18 +0700

    Update Break statement.java

--- Java/Do & Do-While/Break statement.java
+++ Java/Do & Do-While/Break statement.java
@@ -1,8 +1,26 @@
+//Cach 1
+import java.util.Scanner;
+
+public class Loop {
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			if (i == k) {
+				break;
+			}
+			System.out.print(i + " ");
+		}
+	}
+}
+
+//Cach 2
 import java.util.Scanner;
 
 public class Loop {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);        
         int n = sc.nextInt();
         int k = sc.nextInt();
         int i = 1;
