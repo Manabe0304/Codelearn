e162add5bcc86b24ccfb247913d500503c58e2e3
Manabe0304 <hainamn833@gmail.com>
2024-08-21 18:48:31 +0700

    Create Divisible.java

---
+++ Java/Do & Do-While/Divisible.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Loop {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int n = sc.nextInt();
+		while (a <= b) {
+			if (a % n == 0 && a % n == 0) {
+				System.out.print(a + " ");
+			}
+			a++;
+		}
+	}
+}
