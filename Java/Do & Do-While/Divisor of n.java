055e5280e9c1b3dbd7061ce5617e7bd7bc936765
Manabe0304 <hainamn833@gmail.com>
2024-08-21 18:05:56 +0700

    Update Divisor of n.java

--- Java/Do & Do-While/Divisor of n.java
+++ Java/Do & Do-While/Divisor of n.java
@@ -4,14 +4,14 @@ public class Loop {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-        int i = 1;
-        int answer = 0;
-        while(i <= n) {
-            if(n % i == 0) {
-                answer++;
-            }
-            i++;
-        }
-        System.out.println(answer);
+		int i = 1;
+		int answer = 0;
+		while(i <= n) {
+		      if(n % i == 0) {
+                           answer++;
+                      }
+                      i++;
+                }
+		System.out.println(answer);
 	}
 }
