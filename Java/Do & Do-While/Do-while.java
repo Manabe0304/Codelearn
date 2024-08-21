f79a375fe88d763433534c67ebb9c5df6188a40d
Manabe0304 <hainamn833@gmail.com>
2024-08-21 22:45:35 +0700

    Update Do-while.java

--- Java/Do & Do-While/Do-while.java
+++ Java/Do & Do-While/Do-while.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Loop {
-	public static void main(String[] args) {
-		int i = 1;
-		do {
-			System.out.print(i + " ");
-			i++;
-		} while (i <= 5);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int i = 1;
+	do {
+		System.out.print(i + " ");
+		i++;
+	} while (i <= n);
+		
+        System.out.println();
+        i = 1;
+        do {
+	    	if(i % 10 == 0) {
+	    	System.out.print(i + " ");
+	    	}
+	    	i++;
+	} while (i <= n);
+    }
 }
