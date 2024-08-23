8fb37dd040b778733f9e97813d7649c1a6c2fef2
Manabe0304 <hainamn833@gmail.com>
2024-08-23 19:57:30 +0700

    Create Even-Odd.java

---
+++ Java/Array/Even-Odd.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Array {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		//In ra cac so chan trong mang
+		for(int i=0;i<n;i++) {
+			if (arr[i] % 2 == 0) {
+				System.out.print(arr[i] + " ");
+			} 
+		}
+		
+		System.out.println();
+		//In ra cac so le trong mang
+		for(int i=0;i<n;i++) {
+			if (arr[i] % 2 != 0) {
+			    System.out.print(arr[i] + " ");
+			}
+		}	
+	}
+}
