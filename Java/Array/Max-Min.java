2203f574a9b7a7f373d45f2139a807979b353714
Manabe0304 <hainamn833@gmail.com>
2024-08-23 11:48:58 +0700

    Create Max-Min.java

---
+++ Java/Array/Max-Min.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Array {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int maxValue = arr[0];
+        int minValue = arr[0];
+		for (int i = 1; i < n; i++) {
+			if (arr[i] > maxValue) {
+				maxValue = arr[i];
+			}
+		}
+		for (int i = 1; i < n; i++) {
+			if (arr[i] < minValue) {
+				minValue = arr[i];
+			}
+		}
+		System.out.println(maxValue);
+		System.out.println(minValue);
+	}
+}
