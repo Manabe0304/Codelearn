f61a72b103aec8d4f3556c448214bee147df6db5
Manabe0304 <hainamn833@gmail.com>
2024-08-23 22:03:56 +0700

    Update Increase-Decrease.java

--- Java/Array/Increase-Decrease.java
+++ Java/Array/Increase-Decrease.java
@@ -11,16 +11,16 @@ public class Array {
 		}
 		
 		//Sap xep cac so hang trong mang tang dan
-	    for (int i = 0; i < n; i++) {
-	        for (int j = i + 1; j < n; j++) {
-	            if (arr[i] > arr[j]) {
-	                // Neu arr[i] > arr[j], doi cho so hang cua arr[i] va arr[j] cho nhau
-	                int temp = arr[i];
-	                arr[i] = arr[j];
-	                arr[j] = temp;
-	            }
-	        }
-	    }
+	        for (int i = 0; i < n; i++) {
+	             for (int j = i + 1; j < n; j++) {
+	                 if (arr[i] > arr[j]) {
+	                    // Neu arr[i] > arr[j], doi cho so hang cua arr[i] va arr[j] cho nhau
+	                    int temp = arr[i];
+	                    arr[i] = arr[j];
+	                    arr[j] = temp;
+	                 }
+	             }
+	         }
 	    
 		for (int i = 0; i < n; i++) {
 			System.out.print(arr[i] + " ");
@@ -29,16 +29,16 @@ public class Array {
 		System.out.println();
 		
 		//Sap xep cac so hang trong mang hiam dan
-	    for (int i = 0; i < n; i++) {
-	        for (int j = i + 1; j < n; j++) {
-	            if (arr[i] < arr[j]) {
-	                //Neu arr[i] < arr[j], doi cho so hang cua arr[i] va arr[j] cho nhau
-	                int temp = arr[i];
-	                arr[i] = arr[j];
-	                arr[j] = temp;
-	            }
-	        }
-	    }
+	        for (int i = 0; i < n; i++) {
+	             for (int j = i + 1; j < n; j++) {
+	                 if (arr[i] < arr[j]) {
+	                    //Neu arr[i] < arr[j], doi cho so hang cua arr[i] va arr[j] cho nhau
+	                    int temp = arr[i];
+	                    arr[i] = arr[j];
+	                    arr[j] = temp;
+	                 }
+	             }
+	         }
 	    
 		for (int i = 0; i < n; i++) {
 			System.out.print(arr[i] + " ");
