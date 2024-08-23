1df836f14fbfaed8381aa1f806b3d9642bee9d20
Manabe0304 <hainamn833@gmail.com>
2024-08-23 22:34:28 +0700

    Update and rename 2_D array.java to 2-D array.java

--- Java/Array/2-D array.java
+++ Java/Array/2-D array.java
@@ -14,13 +14,13 @@ public class Array {
 			}
 		}
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
+                for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < m; j++) {
+                                System.out.print(arr[i][j] + " ");
+                        }
+                        System.out.println();
+                }
 
-        sc.close();
+                sc.close();
 	}
 }
