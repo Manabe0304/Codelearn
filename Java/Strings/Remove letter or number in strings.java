13b5fc7c0e743a1fce584b5389d59062e411fc4b
Manabe0304 <hainamn833@gmail.com>
2024-08-24 22:22:11 +0700

    Update Remove letter or number in strings.java

--- Java/Strings/Remove letter or number in strings.java
+++ Java/Strings/Remove letter or number in strings.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Array {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
+		String s = sc.nextLine();
 		String s1 = s; 
 		String s2 = s; 
 		for(char c = '0'; c <= '9';c++) {
