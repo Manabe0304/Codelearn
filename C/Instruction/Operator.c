947385e2a4169ebabe6b8a0754103f65695e5a1a
Manabe0304 <hainamn833@gmail.com>
2024-08-25 21:37:27 +0700

    Create Operator.c

---
+++ C/Instruction/Operator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+	printf("1324 + 7553 = %d\n", 1324 + 7553 );
+	printf("4534 - 1227 = %d\n", 4534 - 1227);
+	printf("324 * 335 = %d\n", 324 * 335 );
+	printf("1024 / 64 = %d", 1024 / 64 );
+	return 0;
+}
