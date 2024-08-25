a4854fe46e65757927e69ef7d17bb4821ba24171
Manabe0304 <hainamn833@gmail.com>
2024-08-25 22:29:53 +0700

    Update Char.c

--- C/Variable/Char.c
+++ C/Variable/Char.c
@@ -2,6 +2,21 @@
 
 int main() {
 	char c = 'x';
-	printf("%c", c);
+	printf("%c\n", c);
+	
+	// Chuyen ki tu thanh so nguyen tuong ung
+	printf("a = %d\n", 'a');
+	printf("b = %d\n", 'b');
+	printf("c = %d\n", 'c');
+	printf("d = %d\n", 'd');
+	printf("e = %d\n", 'e');
+	printf("...\n");
+	printf("x = %d\n", 'x');
+	printf("y = %d\n", 'y');
+	printf("z = %d\n", 'z');
+	
+	// Chuyen tu ki tu a sang ki tu d thong qua so nguyen tuong ung
+	char d = 'a' + 3;
+	printf("%c", d);
 	return 0;
 }
