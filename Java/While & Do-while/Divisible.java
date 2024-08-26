e9b54fae8cabfd882c9f7d779311647f0c608dec
Manabe0304 <hainamn833@gmail.com>
2024-08-26 18:59:40 +0700

    Update Divisible.java

--- Java/While & Do-while/Divisible.java
+++ Java/While & Do-while/Divisible.java
@@ -5,9 +5,10 @@ public class Loop {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
+		int m = sc.nextInt();
 		int n = sc.nextInt();
 		while (a <= b) {
-			if (a % n == 0 && a % n == 0) {
+			if (a % m == 0 && a % n == 0) {
 				System.out.print(a + " ");
 			}
 			a++;
