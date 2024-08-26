fafa89055fb8d62497971419fc75e0b9c329fe93
Manabe0304 <hainamn833@gmail.com>
2024-08-26 23:40:18 +0700

    Create Sum element > 0.c

---
+++ C/Array/Sum element > 0.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    int arr[1000];
+    int sum = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 != 0 && arr[i] > 0) {
+            sum += arr[i];
+        }
+    }
+    printf("%d", sum);
+    
+    sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0 && arr[i] > 0) {
+            sum += arr[i];
+        }
+    }
+    printf("\n%d", sum);
+    return 0;
+}
