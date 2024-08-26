6b3389bc38d83e4f62120eef4d639ffa0eb376c6
Manabe0304 <hainamn833@gmail.com>
2024-08-26 21:32:13 +0700

    Create Break statement.c

---
+++ C/While & Do-while/Break statement.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d%d", &n, &k);
+    for (int i = 1; i <= n; i++) {
+        if (i == k) {
+            break;
+        }
+        printf("%d ", i);
+    }
+    return 0;
+}
+
+#include<stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d%d", &n, &k);
+    int i = 1;
+    while (i <= n) {
+        if (i == k) {
+            break;
+        }
+        printf("%d ", i);
+    }
+    return 0;
+}
