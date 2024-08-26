c062b93fc045d566f7635919861ab5c803bd8c61
Manabe0304 <hainamn833@gmail.com>
2024-08-26 18:07:50 +0700

    Create Sum of Odd-Even numbers.c

---
+++ C/Loop/Sum of Odd-Even numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int sum = 0;
+    for (int i = 0; i <= n; i++) {
+        if (i % 2 != 0) {
+            sum += i;
+        }
+    }
+    printf("%d\n", sum);
+    
+    sum = 0;
+    for (int i = 0; i <= n; i++) {
+        if (i % 2 == 0) {
+            sum += i;
+        }
+    }
+    printf("%d\n", sum);
+    return 0;
+}
