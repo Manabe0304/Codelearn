683b16ba2812dcd5eda181b15c52518c4a403cce
Manabe0304 <hainamn833@gmail.com>
2024-08-26 11:07:37 +0700

    Update Compare.c

--- C/If-else/Compare.c
+++ C/If-else/Compare.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-	int a, b;
-	scanf("%d%d", &a, &b);
-	if (a >= b) {
-		printf("a is greater than or equal to b");
-	} else {
-		printf("a is smaller than b");
-	}
+    int a, b;
+    scanf("%d%d", &a, &b);
+    if (a >= b) {
+        printf("a is greater than or equal to b");
+    } else {
+        printf("a is smaller than b");
+    }
 	
     if (a != 0 && b != 0) {
         printf("a is not equal to 0 and b is not equal to 0");
