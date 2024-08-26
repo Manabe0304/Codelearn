fee6d3a3d94e7179e196b01834814115ed969df5
Manabe0304 <hainamn833@gmail.com>
2024-08-26 22:39:52 +0700

    Update Sum.java

--- Java/Array/Sum.java
+++ Java/Array/Sum.java
@@ -7,21 +7,22 @@ public class Array {
 		int[] arr = new int[n];
 		//Tong cua so hang dau va cuoi cua chuoi
 		for (int i = 0; i < n; i++) {
+			System.out.print("a[" + i + "] = " );
 			arr[i] = sc.nextInt();
 		}
-                System.out.println("Sum of the first and last elements: " + (arr[0] + arr[n - 1]));
+        System.out.println("Sum of the first and last elements: " + (arr[0] + arr[n - 1]));
 		
 		//Tong cua cac so hang trong chuoi
-                int sum = 0;
-                for (int i = 0; i < n; i++) {
-                     sum += arr[i];
-                }
-                System.out.println("Sum of all elements: " + sum);
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Sum of all elements: " + sum);
         
-                //Dao nguoc cac so hang trong chuoi
-                System.out.print("Array in reverse order: ");
-                for (int i = n - 1; i >= 0; i--) {
-                     System.out.print(arr[i] + " ");
-                }
+        //Dao nguoc cac so hang trong chuoi
+        System.out.print("Array in reverse order: ");
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
 	}
 }
