52bff2b8d6663f72ccef79e6e624f847210e40de
Manabe0304 <hainamn833@gmail.com>
2024-08-26 12:08:28 +0700

    Update Increase-Decrease.java

--- Java/If-else/Increase-Decrease.java
+++ Java/If-else/Increase-Decrease.java
@@ -6,12 +6,12 @@ public class Statement {
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
-	    if (a <= b && b <= c) {
-	        System.out.println("increasing");
-	    } else if (a >= b && b >= c) {
-	    	System.out.println("decreasing");
-	    } else {
-	    	System.out.println("neither increasing nor decreasing order");
-	    }
+	        if (a <= b && b <= c) {
+	             System.out.println("increasing");
+	        } else if (a >= b && b >= c) {
+	    	     System.out.println("decreasing");
+	        } else {
+	    	     System.out.println("neither increasing nor decreasing order");
+	        }
 	}
 }
