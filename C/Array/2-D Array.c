dccaf750d19bb5f79d457d27a1e81c8ab7ef46ae
Manabe0304 <hainamn833@gmail.com>
2024-08-27 11:01:21 +0700

    Create 2-D Array.c

---
+++ C/Array/2-D Array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+	int arr[100][100];
+	int n, m;
+
+	scanf("%d%d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("a[%d][%d] = ", i, j);
+			scanf("%d", &arr[i][j]);
+		}
+	}
+
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			sum += arr[i][j];
+		}
+	}
+
+	printf("%d", sum);
+
+	return 0;
+}
