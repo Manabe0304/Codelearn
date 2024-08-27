566bc39c0c4b5484a79f9dd0602ed6148035d8d6
Manabe0304 <hainamn833@gmail.com>
2024-08-27 11:44:39 +0700

    Update String.c

--- C/String/String.c
+++ C/String/String.c
@@ -6,3 +6,24 @@ int main() {
 	printf("%s", str);
 	return 0;
 }
+
+//Cach 1
+#include<stdio.h>
+
+int main() {
+    char name[100], address[100];
+    scanf("%s%s", name, address);
+    printf("Name: %s\nAddress: %s", name, address);
+    return 0;
+}
+
+//Cach 2: Co dau cach trong xau
+#include<stdio.h>
+
+int main() {
+    char name[100], address[100];
+    gets(name);
+    gets(address);
+    printf("Name: %s\nAddress: %s", name, address);
+    return 0;
+}
