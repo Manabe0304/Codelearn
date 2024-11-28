e9ed5b43db0cce902b616323b2546b235aa57fe1
Manabe0304 <hainamn833@gmail.com>
2024-11-28 11:40:03 +0700

    Create Entry.java

---
+++ OOP in Java/Encapsulation/Date/Entry.java
@@ -0,0 +1,10 @@
+public class Entry {
+	public static void main(String[] args) {
+		Date d = new Date(1, 1, 1997);
+		d.display();
+		d.setDate(12, 12, 2004);
+		d.display();
+		d.setMonth(3);
+		d.display();
+	}
+}
