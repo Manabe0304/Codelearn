61cdba7932c49489b4b47e3ccf1ec6e5b34b9811
Manabe0304 <hainamn833@gmail.com>
2024-12-16 11:39:58 +0700

    Create transcript.sql

---
+++ Introduction to SQL/Query of the data/ROUND function/transcript.sql
@@ -0,0 +1,15 @@
+CREATE TABLE transcript (
+  id SMALLINT,
+  point SMALLINT
+);
+
+INSERT into multiplication (a, b)
+VALUES 
+(1, 8.333), 
+(2, 9.55), 
+(3, 10);
+
+SELECT 
+id,ROUND(point,2) AS point
+FROM 
+transcript;
