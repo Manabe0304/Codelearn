edd96af5e3833bb3f71f83adf61dc8bb4a274754
Manabe0304 <hainamn833@gmail.com>
2025-09-04 21:55:20 +0700

    Create print.py

---
+++ Python/Instruction/print.py
@@ -0,0 +1,11 @@
+# Cách 1
+
+arg1 = "Welcome to Codelearn.io!" # Gán chuỗi vào biến arg1
+
+def helloWorld(arg1): # Định nghĩa hàm helloWorld với tham số arg1.
+    print(arg1) # In nội dung của arg1.
+
+helloWorld(arg1) # Gọi hàm và truyền vào biến arg1.
+
+# Cách 2
+print("Welcome to Codelearn.io!)
