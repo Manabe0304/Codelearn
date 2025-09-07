e7a5f13e90f5d32b536e9e52138ab8ee82c28aca
Manabe0304 <hainamn833@gmail.com>
2025-09-07 21:23:52 +0700

    Update Int.java

--- Java/Variables and Data types/Int.java
+++ Java/Variables and Data types/Int.java
@@ -1,6 +1,6 @@
 public class Variable {
      public static void main(String[] args) {
-	int a = 6;
+	    int a = 6;
         int b = 2;
         System.out.println("a + b = " + (a + b));
         System.out.println("a - b = " + (a - b));
