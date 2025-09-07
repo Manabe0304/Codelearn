f6f8c4ddb1a5982816ff492c62178ae1cd864af0
Manabe0304 <hainamn833@gmail.com>
2025-09-07 22:37:50 +0700

    Create Operator.py

---
+++ Python/Data type, variable and type conversion/Operator.py
@@ -0,0 +1,12 @@
+a = 438
+b = 636
+print("a + b = " + str(a + b))
+print("a - b = " + str(a - b))
+print("a * b = " + str(a * b))
+print("a / b = " + str(a / b))
+
+length = 7.8
+width = 3.5
+
+print("Area: " + str(length * width))
+print("Perimeter: " + str((length + width) *2))
