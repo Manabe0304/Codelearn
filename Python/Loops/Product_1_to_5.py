fe610aa964edccc9ec521cf80c0174cab300b253
Manabe0304 <hainamn833@gmail.com>
2025-11-21 22:17:47 +0700

    Create Product_1_to_5.py

---
+++ Python/Loops/Product_1_to_5.py
@@ -0,0 +1,13 @@
+# While-loop
+n = int(input())
+i = 1
+
+while i <= 5:
+    print(n, "*", i, "=", n * i)
+    i += 1
+
+# For-loop
+n = int(input())
+
+for i in range(1, 6):
+    print(n, "*", i, "=", n * i)
