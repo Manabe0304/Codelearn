2a186d4af11c13e98c269dd8e6270d6bf3d9d366
Manabe0304 <hainamn833@gmail.com>
2025-11-22 23:42:51 +0700

    Create max-min.py

---
+++ Python/Python Function/max-min.py
@@ -0,0 +1,15 @@
+def max3(a, b, c):
+    if a > b and a > c:
+        return a
+    return b if b > c else c
+
+def min3(a, b, c):
+    if a < b and a < c:
+        return a 
+    return b if b < c else c
+
+a = int(input())
+b = int(input())
+c = int(input())
+print(max3(a, b, c))
+print(min3(a, b, c))
