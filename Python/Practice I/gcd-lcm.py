7d2f0bbcadc558c667786ffc2b0a220d5bd895af
Manabe0304 <hainamn833@gmail.com>
2025-11-23 09:54:59 +0700

    Create gcd-lcm.py

---
+++ Python/Practice I/gcd-lcm.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+print(lcm(a, b))
