21ad8d5130511df07eee252de9dc62f71c4fa081
Manabe0304 <hainamn833@gmail.com>
2026-01-12 23:19:31 +0700

    Update HelloWorld.cs

--- C#/Basic C#/HelloWorld.cs
+++ C#/Basic C#/HelloWorld.cs
@@ -4,6 +4,14 @@ namespace HelloWorld {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Codelearn.io!");
+
+            // Cách 1
+            Console.WriteLine("Name: Codelearn");
+            Console.WriteLine("Date of birth: 2019");
+
+            // Cách 2
+            Console.WriteLine("Name: Codelearn\nDate of birth: 2019");
         }
     }
 }
