c4ed69bb28a4c7058512b798b49836d46d594f98
Manabe0304 <hainamn833@gmail.com>
2026-01-12 23:21:42 +0700

    Create Operator.cs

---
+++ C#/Basic C#/Operator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(8 + 4);
+            Console.WriteLine(8 - 4);
+            Console.WriteLine(8 * 4);
+            Console.WriteLine(8 / 4);
+        }
+    }
+}
