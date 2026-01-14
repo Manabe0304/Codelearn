0bd8a58ab1a4f155d68f86afd5d6f6250a16d68c
Manabe0304 <hainamn833@gmail.com>
2026-01-14 10:41:51 +0700

    Create convert_to_UpperLine.cs

---
+++ C#/Strings/convert_to_UpperLine.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace String {
+    class Program {
+        static void Main(string[] args) {
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            s1 = s1.ToLower();
+            s2 = s2.ToLower();
+            Console.WriteLine(s1.IndexOf(s2));
+		}
+    }
+}
