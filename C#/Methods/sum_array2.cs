311f8d29b266d2badd5cf843b1d199c429e0e932
Manabe0304 <hainamn833@gmail.com>
2026-01-17 17:00:16 +0700

    Create sum_array2.cs

---
+++ C#/Methods/sum_array2.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Method {
+    class Program {
+        public static int SumOfArray(int[] arr, int n) {
+            if (n == 1) return arr[0];
+            return arr[n - 1] + SumOfArray(arr, n - 1);
+        }
+
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write(SumOfArray(arr, n));
+        }
+    }
+}
