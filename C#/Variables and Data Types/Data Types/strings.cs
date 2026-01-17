784b7a181b599b219329481805f6862a975c99f1
Manabe0304 <hainamn833@gmail.com>
2026-01-17 17:51:48 +0700

    Update strings.cs

--- C#/Variables and Data Types/Data Types/strings.cs
+++ C#/Variables and Data Types/Data Types/strings.cs
@@ -4,7 +4,9 @@ namespace Variable {
     class Program {
         static void Main(string[] args) {
             string name = "Codelearn";
-            Console.WriteLine("Hello " + name);
+            int dob = 2019;
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Date of birth: " + dob);
         }
     }
 }
